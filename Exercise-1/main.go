22a22314b0feb039b25d7d1d82dacb64661767ec
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-08 14:43:47 +0100

    Update main.go

--- Exercise-1/main.go
+++ Exercise-1/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
+	csvFileName := flag.String("csv", "problems.csv", "A csv file in question-answer format")
+	flag.Parse()
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		fmt.Errorf("Couldn't open the file %s\n", *csvFileName)
+		os.Exit(1)
+	}
+	_ = file
 
 }
