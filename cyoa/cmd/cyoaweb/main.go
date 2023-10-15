7d98899d9e7b06f7de1d9e68570c951f97e6590a
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-15 16:03:40 +0100

    Update main.go

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -30,8 +30,12 @@ func main() {
 
 	tpl := template.Must(template.New("").Parse(storyTmpl))
 	h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl), cyoa.WithPathFunc(pathFn))
+
+	mux := http.NewServeMux()
+	mux.Handle("/story", h)
+
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
 
 func pathFn(r *http.Request) string {
