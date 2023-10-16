edbd9297f58d5b0bf87ee5aae58736260d462e88
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-16 08:50:00 +0100

    Update parse.go

--- Exercise-4/link/parse.go
+++ Exercise-4/link/parse.go
@@ -1,7 +1,13 @@
 package link
 
+import "io"
+
 //Link represents a link in a HTML doc
 type Link struct {
 	Href string
 	Text string
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	return nil, nil
 }
