6fb8a9696c2ef3df51da866fab99d12d6696ccd6
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-24 16:27:01 +0100

    Update main.go

--- Exercise-5/sitemap_builder/main.go
+++ Exercise-5/sitemap_builder/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/MrYanev/Gophercises/Exercise-4/link"
 )
@@ -30,12 +32,28 @@ func main() {
 	defer resp.Body.Close()
 	io.Copy(os.Stdout, resp.Body)
 
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
 	links, err := link.Parse(resp.Body)
 	if err != nil {
 		fmt.Printf("There is an %s error", err)
 	}
-	for _, l := range links {
-		fmt.Println(l)
-	}
 
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
