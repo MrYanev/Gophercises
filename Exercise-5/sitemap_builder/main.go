be65faf34c20b4ff1c3a1e8475255de693e50d4a
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-24 16:09:53 +0100

    Update main.go

--- Exercise-5/sitemap_builder/main.go
+++ Exercise-5/sitemap_builder/main.go
@@ -6,9 +6,19 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/MrYanev/Gophercises/Exercise-4/link"
 )
 
 func main() {
+	/*
+		1. Get the webpage
+		2. Parse all the links on the page
+		3. Build proper urls with the links
+		4.Filter out any links w/ a diff domain
+		5. Find all pages
+		6. Print out the XML
+	*/
 	urlFlag := flag.String("url", "https://gophercises.com", "the URL to buld the site map for")
 	flag.Parse()
 
@@ -19,12 +29,13 @@ func main() {
 	}
 	defer resp.Body.Close()
 	io.Copy(os.Stdout, resp.Body)
-	/*
-		1. Get the webpage
-		2. Parse all the links on the page
-		3. Build proper urls with the links
-		4.Filter out any links w/ a diff domain
-		5. Find all pages
-		6. Print out the XML
-	*/
+
+	links, err := link.Parse(resp.Body)
+	if err != nil {
+		fmt.Errorf("There is an %s error", err)
+	}
+	for _, l := range links {
+		fmt.Println(l)
+	}
+
 }
