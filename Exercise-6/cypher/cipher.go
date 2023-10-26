69cb30a2ef1b7c6975d101be28714f270e0a1b92
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-26 15:29:50 +0100

    Update cipher.go

--- Exercise-6/cypher/cipher.go
+++ Exercise-6/cypher/cipher.go
@@ -1,15 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var shift int
 	var input, ans string
 	fmt.Scanf("%s\n", &input)
 	fmt.Scanf("%v\n", &shift)
-	for _, char := range input {
-		shifted := int(char) + shift
-		ans += fmt.Sprint(shifted)
+	// for _, char := range input {
+	// 	shifted := int(char) + shift
+	// 	ans += fmt.Sprint(shifted)
+	// }
+	alphabetStr := "abcdefghijklmnopqrstuvwxyz"
+	alphabet := []rune(alphabetStr)
+	for _, ch := range input {
+		if strings.IndexRune(alphabetStr, ch) >= 0 {
+			ans += string(rotate(ch, shift, alphabet))
+		} else {
+			ans += string(ch)
+		}
 	}
 	fmt.Println(ans)
+
+}
+
+func rotate(s rune, delta int, key []rune) rune {
+	idx := strings.IndexRune(string(key), s)
+	if idx < 0 {
+		panic("idx < 0")
+	}
+	idx = (idx + delta) % len(key)
+	return key[idx]
 }
