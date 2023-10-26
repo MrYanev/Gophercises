2b45390682a7134290832ff9d39df7e2056c1147
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-26 08:32:48 +0100

    Update main.go

--- Exercise-5/sitemap_builder/main.go
+++ Exercise-5/sitemap_builder/main.go
@@ -13,12 +13,15 @@ import (
 	"github.com/MrYanev/Gophercises/Exercise-4/link"
 )
 
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
 type loc struct {
 	Value string `xml:"loc"`
 }
 
 type urlset struct {
-	Urls []loc `xml:"url"`
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,arrt"`
 }
 
 func main() {
@@ -35,11 +38,15 @@ func main() {
 	flag.Parse()
 
 	pages := bfs(*urlFlag, *maxDepth)
-	var toXml urlset
+	toXml := urlset{
+		Xmlns: xmlns,
+	}
 	for _, page := range pages {
 		toXml.Urls = append(toXml.Urls, loc{page})
 	}
+	fmt.Print(xml.Header)
 	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", " ")
 	if err := enc.Encode(toXml); err != nil {
 		panic(err)
 	}
@@ -56,13 +63,18 @@ func bfs(urlStr string, maxDepth int) []string {
 	}
 	for i := 0; i <= maxDepth; i++ {
 		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
 		for url, _ := range q {
 			if _, ok := seen[url]; ok {
 				continue
 			}
 			seen[url] = struct{}{}
 			for _, link := range get(url) {
-				nq[link] = struct{}{}
+				if _, ok := seen[link]; !ok {
+					nq[link] = struct{}{}
+				}
 			}
 		}
 	}
