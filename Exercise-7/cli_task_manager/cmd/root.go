66bb741167741ccddc5ca8aedf6ee2aea2f874b3
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-28 09:01:38 +0100

    Update root.go

--- Exercise-7/cli_task_manager/cmd/root.go
+++ Exercise-7/cli_task_manager/cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-//By not adding a func to the variable it makes it list all other commands
 var RootCmd = &cobra.Command{
 	Use:   "task",
 	Short: "Task is CLI task mannager",
