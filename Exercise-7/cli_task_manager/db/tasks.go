718e471f8644b75dae3149229b191169f215671a
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-29 12:48:37 +0000

    Update tasks.go

--- Exercise-7/cli_task_manager/db/tasks.go
+++ Exercise-7/cli_task_manager/db/tasks.go
@@ -6,10 +6,30 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-func main() {
-	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+type Task struct {
+	Key   int
+	Value string
 }
+
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
+}
+
+// func main() {
+// 	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer db.Close()
+// }
