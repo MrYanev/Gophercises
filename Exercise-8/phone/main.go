c4a1eb8f1d8bb729d3b65099c8db1c8dc0b8ca16
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-30 16:44:32 +0000

    Update main.go

--- Exercise-8/phone/main.go
+++ Exercise-8/phone/main.go
@@ -1,9 +1,17 @@
 package main
 
+import "bytes"
+
 func main() {
 
 }
 
 func normalize(phone string) string {
-	return phone
+	var buf bytes.Buffer
+	for _, i := range phone {
+		if i >= '0' && i <= '9' {
+			buf.WriteRune(i)
+		}
+	}
+	return buf.String()
 }
