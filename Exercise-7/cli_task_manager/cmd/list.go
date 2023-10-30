5da4aad717fdaf9af2704835ee4075e79992fa81
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-30 14:57:27 +0000

    Update list.go

--- Exercise-7/cli_task_manager/cmd/list.go
+++ Exercise-7/cli_task_manager/cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/MrYanev/Gophercises/Exercise-7/cli_task_manager/db"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +12,16 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("List called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong")
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks left")
+		}
+
+		fmt.Println("You have the following tasks")
 	},
 }
 
