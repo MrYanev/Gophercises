f7660aa43c4f9984aaacec564097472a91567f7e
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-30 15:05:46 +0000

    Update add.go

--- Exercise-7/cli_task_manager/cmd/add.go
+++ Exercise-7/cli_task_manager/cmd/add.go
@@ -5,8 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/MrYanev/Gophercises/Exercise-7/cli_task_manager/db"
 	"github.com/spf13/cobra"
 )
 
@@ -16,6 +18,11 @@ var addCmd = &cobra.Command{
 	Short: "Add task to the list",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Something went wrong", err.Error())
+			os.Exit(1)
+		}
 		fmt.Printf("Added \"%s\" to your task list.\n", task)
 	},
 }
