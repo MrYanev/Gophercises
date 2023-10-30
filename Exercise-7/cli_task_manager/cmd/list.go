a476b8ba6e15a2985a4ef340c779ae4eb4d9ff9a
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-30 15:01:45 +0000

    Update list.go

--- Exercise-7/cli_task_manager/cmd/list.go
+++ Exercise-7/cli_task_manager/cmd/list.go
@@ -19,9 +19,12 @@ var listCmd = &cobra.Command{
 		}
 		if len(tasks) == 0 {
 			fmt.Println("You have no tasks left")
+			return
 		}
-
 		fmt.Println("You have the following tasks")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
