f3e39c1314a0247f720410577c97702a2035193c
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-01 16:07:42 +0000

    Create phone.go

---
+++ Exercise-8/phone/db/phone.go
@@ -0,0 +1,53 @@
+package db
+
+import "database/sql"
+
+func Reset(driverName, dataSource, dbName string) error {
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return err
+	}
+	err = resetDB(db, dbName)
+	if err != nil {
+		return err
+	}
+	return db.Close()
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name) //Has a sql injection flow don't use
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Migrate(driverName, dataSource string) error {
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return err
+	}
+	err = createPNTable(db)
+	if err != nil {
+		return err
+	}
+	return db.Close()
+}
+
+func createPNTable(db *sql.DB) error {
+	statement := `
+	CREATE TABLE IF NOT EXISTS phone_numbers (
+		id SERIAL,
+		value VARCHAR(255),
+	)`
+	_, err := db.Exec(statement)
+	return err
+}
