71a8b7c0b2433a61ef5f18c5aa83df76126f1d9d
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-03 13:24:56 +0000

    Update card.go

--- Exercise-9/deck_of_cards/deck/card.go
+++ Exercise-9/deck_of_cards/deck/card.go
@@ -1,4 +1,19 @@
 package deck
 
-type Cart struct {
+type Card struct {
+}
+
+// A function to Shuffle the deck
+func (c *Card) Shuffle([]Card) ([]Card, error) {
+	return nil, nil
+}
+
+// A function to generate new deck of cards
+func NewDeck() []Card {
+	return nil
+}
+
+// A function to remove specific type of cards
+func (c *Card) SortOut(deck []Card, suit string) ([]Card, error) {
+	return nil, nil
 }
