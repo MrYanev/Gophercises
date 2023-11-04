f2f9be4e8a2dc515b483ab571e0eebb3fd9f61e5
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-04 15:03:54 +0000

    Update card_test.go

--- Exercise-9/deck_of_cards/deck/card_test.go
+++ Exercise-9/deck_of_cards/deck/card_test.go
@@ -8,3 +8,11 @@ func TestNew(t *testing.T) {
 		t.Error("Wrong amount of cards")
 	}
 }
+
+func TestDefaultSort(t *testing.T) {
+	cards := NewDeck(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades, received card:", cards[0])
+	}
+}
