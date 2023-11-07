3a0ad971ac58c9c5ff0193e9004de73faf9e8c6d
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-07 15:33:52 +0000

    Update main.go

--- Exercise-10/blackjack/main.go
+++ Exercise-10/blackjack/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/MrYanev/Gophercises/Exercise-9/deck_of_cards/deck"
+)
+
+func main() {
+	cards := deck.NewDeck(deck.Deck(3), deck.Shuffle)
+	var card deck.Card
+	for i := 0; i < 10; i++ {
+		card, cards = cards[0], cards[1:]
+		fmt.Println(card)
+	}
 }
