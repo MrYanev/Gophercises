f2c155c86916abb5f589e4451c7a5ea3bcb68e4f
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-17 12:42:58 +0000

    Update ai.go

--- Exercise-11/blackjack_ai/blackjack/ai.go
+++ Exercise-11/blackjack_ai/blackjack/ai.go
@@ -1,4 +1,48 @@
 package blackjack
 
+import (
+	"fmt"
+
+	"github.com/MrYanev/Gophercises/Exercise-9/deck_of_cards/deck"
+)
+
 type AI interface {
+}
+
+type HumanAI struct{}
+
+func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) {
+	for {
+		fmt.Println("Player: ", hand)
+		fmt.Println("Dealer: ", dealer)
+		fmt.Println("What will you do? (h)it, (s)tands?")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return Hit
+		case "s":
+			return Stand
+		default:
+			fmt.Println("That's not a valid option!")
+		}
+	}
+}
+
+func (ai *HumanAI) Results(hand []deck.Card, dealer []deck.Card) {
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player: ", ret.Player, "\nScore:", pScore)
+	fmt.Println("Dealer: ", ret.Dealer, "\nScore:", dScore)
+}
+
+type Moce func(GameState) GameState{}
+
+type GameState struct{}
+
+func Hit(gs GameState) GameState {
+	return gs
+}
+
+func Stand(gs GameState) GameState {
+	return gs
 }
