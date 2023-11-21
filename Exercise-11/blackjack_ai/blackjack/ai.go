0482476ddfdbc0153e18070169f6bbc006033f28
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-21 13:56:36 +0000

    Update ai.go

--- Exercise-11/blackjack_ai/blackjack/ai.go
+++ Exercise-11/blackjack_ai/blackjack/ai.go
@@ -9,35 +9,38 @@ import (
 type AI interface {
 	Bet() int
 	Results(hand [][]deck.Card, dealer []deck.Card)
-	Play(hand []deck.Card, dealer deck.Card)
+	Play(hand []deck.Card, dealer deck.Card) Move
 }
-
-type HumanAI struct{}
 
 type dealerAI struct{}
 
-func (ai *dealerAI) Bet() int {
+func (ai dealerAI) Bet() int {
 	return 1
 }
 
-func (ai *dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
+func (ai dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	dScore := Score(hand...)
 	if dScore <= 16 || (dScore == 17 && Soft(hand...)) {
 		return MoveHit
-	} else {
-		return MoveStand
 	}
+	return MoveStand
 }
 
-func (ai *dealerAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+func (ai dealerAI) Results(hand [][]deck.Card, dealer []deck.Card) {
 	//Noop
 }
 
-func (ai *HumanAI) Bet() int {
+func HumanAI() AI {
+	return humanAI{}
+}
+
+type humanAI struct{}
+
+func (ai humanAI) Bet() int {
 	return 1
 }
 
-func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) {
+func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	for {
 		fmt.Println("Player: ", hand)
 		fmt.Println("Dealer: ", dealer)
@@ -46,16 +49,16 @@ func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) {
 		fmt.Scanf("%s\n", &input)
 		switch input {
 		case "h":
-			return MoveHit()
+			return MoveHit
 		case "s":
-			return MoveStand()
+			return MoveStand
 		default:
 			fmt.Println("That's not a valid option!")
 		}
 	}
 }
 
-func (ai *HumanAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+func (ai humanAI) Results(hands [][]deck.Card, dealer []deck.Card) {
 	fmt.Println("==FINAL HANDS==")
 	fmt.Println("Player:")
 	for _, h := range hands {
