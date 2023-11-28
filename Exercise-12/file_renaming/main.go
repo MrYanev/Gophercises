993f21bbf10d6048bcc57e3623a58b7e5aa9b09f
Atanas Yanev <atanasyanev2@gmail.com>
2023-11-28 15:01:57 +0000

    Update main.go

--- Exercise-12/file_renaming/main.go
+++ Exercise-12/file_renaming/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	fileName := "birthday_001.txt"
+	newName, err := match(fileName)
+	if err != nil {
+		fmt.Println("no match")
+		os.Exit(1)
+	}
+	fmt.Println(newName)
 
 }
 
